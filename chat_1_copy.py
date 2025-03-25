908e944de4769c171edd72f03a9f87109e174555
Shabana Kausar <88205538+shabanakausar@users.noreply.github.com>
2025-03-25 13:39:15 -0700

    Add files via upload

--- chat_1_copy.py
+++ chat_1_copy.py
@@ -13,7 +13,7 @@
 
 import streamlit as st
 import os
-from dotenv import load_dotenv
+from python-dotenv import load_dotenv
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.llms import Ollama
